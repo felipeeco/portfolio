49e6bb65b56adb6418d4d5195edb2265742193ff
felipeeco <contacto@felipemoreno.co>
2023-11-19 20:55:47 -0500

    I did layout

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { notFound } from "next/navigation";
 import { useLocale } from "next-intl";
+import Image from "next/image";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -23,7 +24,81 @@ export default function RootLayout({
 
   return (
     <html lang={locale}>
-      <body>{children}</body>
+      <body>
+        <div className="layout">
+          {/* Sidebar */}
+          <aside className="layout__aside">
+            <section className="aside__user-info">
+              {/* Info user */}
+              <div className="user-info__general-info">
+                <div className="user-info__container-image">
+                  <Image
+                    className="user-info__image"
+                    src="/img/default-user.jpg"
+                    alt="profile photo"
+                    width={180}
+                    height={180}
+                  />
+                </div>
+                <h2 className="user-info__name">Felipe Moreno</h2>
+                <h1 className="user-info__job">Frontend Developer</h1>
+              </div>
+
+              {/* Nav */}
+              <nav className="layout__menu">
+                <ul className="menu__list">
+                  <li className="menu__option">
+                    <a
+                      href="index.html"
+                      className="menu__link menu__icon--active"
+                    >
+                      <i className="fa-solid fa-house menu__icon"></i>
+                      <span className="menu__overlay">Home</span>
+                    </a>
+                  </li>
+                  <li className="menu__option">
+                    <a href="portfolio.html" className="menu__link">
+                      <i className="fa-solid fa-briefcase menu__icon"></i>
+                      <span className="menu__overlay">Portafolio</span>
+                    </a>
+                  </li>
+                </ul>
+              </nav>
+
+              {/* Social media links */}
+              <div className="user-info__links">
+                <ul className="links__social">
+                  <li className="social__option">
+                    <a href="#">
+                      <i className="fa-brands fa-linkedin"></i>
+                    </a>
+                  </li>
+                  <li className="social__option">
+                    <a href="#">
+                      <i className="fa-brands fa-github"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+
+              {/*  Buton */}
+              {/* <div className="user-info__buttons">
+                    <a href="#" className="user-info__btn">
+                        Descargar CV
+                    </a>
+                </div> */}
+            </section>
+          </aside>
+
+          {/* Responsive menu */}
+          <div className="layout__menu-toggle">
+            <i className="menu-toggle__icon fa-solid fa-bars"></i>
+            <i className="menu-toggle__icon fa-solid fa-xmark"></i>
+          </div>
+          {/* Main Content */}
+          <main className="layout__content">{children}</main>
+        </div>
+      </body>
     </html>
   );
 }
--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -4,8 +4,150 @@ export default function Home() {
   const t = useTranslations("Index");
 
   return (
-    <main>
-      <h1>{t("title")}</h1>
-    </main>
+    <article className="content__page content__about">
+      <header className="about__header">
+        <h2 className="about__title">
+          Sobre <span className="title__color">Mi</span>
+        </h2>
+      </header>
+      <section className="about__personal-info">
+        <article className="personal-info__bio">
+          <p className="personal-info__description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+            voluptatum minus, ratione, commodi animi omnis et unde quis sunt
+            laboriosam reiciendis saepe! Eius minima alias porro sint harum.
+            Consequatur, minima?
+          </p>
+        </article>
+      </section>
+      <section className="experience__knowledges">
+        <header className="knowledges__subheader">
+          <h2 className="experience__subtitle">Technical Skilss</h2>
+        </header>
+        <section className="knowledges__container">
+          <ul className="knowledges__list">
+            <li className="knowledges__option">JavaScript</li>
+            <li className="knowledges__option">JavaScript</li>
+            <li className="knowledges__option">JavaScript</li>
+            <li className="knowledges__option">JavaScript</li>
+            <li className="knowledges__option">JavaScript</li>
+            <li className="knowledges__option">JavaScript</li>
+            <li className="knowledges__option">JavaScript</li>
+          </ul>
+        </section>
+      </section>
+      <section className="experience__container">
+        <section className="experience__left">
+          <header className="experience__subheader">
+            <h2 className="experience__subtitle">Experiencia</h2>
+          </header>
+          <div className="experience__timelines">
+            <article className="timelines__timeline">
+              <header className="timeline__header">
+                <h3 className="timeline__year">2010</h3>
+                <span className="timelinecompany">Indra</span>
+              </header>
+
+              <div className="timeline__divider"></div>
+
+              <div className="timeline__description">
+                <h3 className="timeline__title">Desarrollador Frontend</h3>
+                <p className="timeline__text">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis
+                  odit repellendus non debitis architecto, at voluptatum magnam
+                  adipisci? Quia rerum omnis veniam eius culpa unde, error
+                  dolores vel obcaecati quod.
+                </p>
+              </div>
+            </article>
+            <article className="timelines__timeline">
+              <header className="timeline__header">
+                <h3 className="timeline__year">2010</h3>
+                <span className="timelinecompany">Indra</span>
+              </header>
+
+              <div className="timeline__divider"></div>
+
+              <div className="timeline__description">
+                <h3 className="timeline__title">Desarrollador Frontend</h3>
+                <p className="timeline__text">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis
+                  odit repellendus non debitis architecto, at voluptatum magnam
+                  adipisci? Quia rerum omnis veniam eius culpa unde, error
+                  dolores vel obcaecati quod.
+                </p>
+              </div>
+            </article>
+            <article className="timelines__timeline">
+              <header className="timeline__header">
+                <h3 className="timeline__year">2010</h3>
+                <span className="timelinecompany">Indra</span>
+              </header>
+
+              <div className="timeline__divider timeline__divider--last"></div>
+
+              <div className="timeline__description">
+                <h3 className="timeline__title">Desarrollador Frontend</h3>
+                <p className="timeline__text">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis
+                  odit repellendus non debitis architecto, at voluptatum magnam
+                  adipisci? Quia rerum omnis veniam eius culpa unde, error
+                  dolores vel obcaecati quod.
+                </p>
+              </div>
+            </article>
+          </div>
+        </section>
+      </section>
+      <section className="experience__certificates">
+        <header className="experience__subheader">
+          <h2 className="experience__subtitle">Certificados</h2>
+        </header>
+        <section className="certificates__container">
+          <article className="certificate__item">
+            <div className="certificate__logo">
+              <img
+                className="certificate__image"
+                src="/img/certificado-1.png"
+                alt="imagen"
+              />
+            </div>
+            <div className="certificate__content">
+              <h4 className="certificate__title">Node.js certificado</h4>
+              <span className="certificate__id">545345</span>
+              <span className="certificate__date">23 de agosto de 2023</span>
+            </div>
+          </article>
+          <article className="certificate__item">
+            <div className="certificate__logo">
+              <img
+                className="certificate__image"
+                src="/img/certificado-1.png"
+                alt="imagen"
+              />
+            </div>
+            <div className="certificate__content">
+              <h4 className="certificate__title">Node.js certificado</h4>
+              <span className="certificate__id">545345</span>
+              <span className="certificate__date">23 de agosto de 2023</span>
+            </div>
+          </article>
+          <article className="certificate__item">
+            <div className="certificate__logo">
+              <img
+                className="certificate__image"
+                src="/img/certificado-1.png"
+                alt="imagen"
+              />
+            </div>
+            <div className="certificate__content">
+              <h4 className="certificate__title">Node.js certificado</h4>
+              <span className="certificate__id">545345</span>
+              <span className="certificate__date">23 de agosto de 2023</span>
+            </div>
+          </article>
+        </section>
+      </section>
+    </article>
   );
 }
